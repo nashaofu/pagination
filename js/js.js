21752487a9967ce4309a5752c5d5b8f25243adee
程刁 <diaocheng@outlook.com>
2016-08-27 14:48:02 +0800

    添加新的翻页按钮模式js.js

--- js/js.js
+++ js/js.js
@@ -1,9 +1,3 @@
-/**
- * 黑桃Lab 分页插件 Pagination
- * 插件支持IE7及其以上版本浏览器
- * @param  {[type]} factory [description]
- * @return {[type]}         [description]
- */
 (function(factory) {
     "use strict";
     if (typeof define === "function" && define.amd) {
@@ -20,11 +14,6 @@
     }
 })(function($) {
     "use strict";
-    /**
-     * 分页按钮构造函数
-     * @param {[type]} $target [description]
-     * @param {[type]} options [description]
-     */
     var Pagination = function($target, options) {
         this.$target = $target;
         this.options = $.extend({}, Pagination.DEFAULTS, this.$target.data("pagination"), typeof options == "object" && options);
@@ -49,37 +38,34 @@
         total: 1, // 页码总数
         active: 1, // 当前页码
         size: 2, // 当前页码两边显示页码数量
+        first: "first", //首页
+        last: "last", //最后一页
         prev: "&lt;", // 上一页默认符号
         next: "&gt;", // 下一页默认符号
-        click: function(active, $target) {} // 点击回调函数
+        click: function(active, $target) {} //点击回调函数
     };
-    // 插件原型方法
     Pagination.prototype = {
-        // 构建html
+        init: function() {},
         build: function() {
             var me = this,
                 $target = me.$target,
                 options = me.options;
-            // 清空分页按钮
+            //清空分页按钮
             $target.empty();
+            // 首页
+            $target.append('<li><a herf="javascript:void(0)" data-page="first">' + options.first + '</a></li>');
             // 上一页
             $target.append('<li><a herf="javascript:void(0)" data-page="prev">' + options.prev + '</a></li>');
-            // 第一页
-            $target.append('<li><a herf="javascript:void(0)" data-page="1">1</a></li>');
+            // 中间页码
             // 生成中间页码
             var size = me.getSize();
             for (var i = size.start; i <= size.end; i++) {
                 $target.append('<li><a herf="javascript:void(0)" data-page="' + i + '">' + i + '</a></li>');
             }
-            // 最后一页
-            $target.append('<li><a herf="javascript:void(0)" data-page="' + options.total + '">' + options.total + '</a></li>');
             // 下一页
             $target.append('<li><a herf="javascript:void(0)" data-page="next">' + options.next + '</a></li>');
-            // 生成省略页码
-            var ellipsis = me.getEllipsis();
-            for (var i = 0; i < ellipsis.length; i++) {
-                $target.find('li>a[data-page="' + ellipsis[i] + '"]').parent("li").after('<li><span>...</span></li>');
-            }
+            // 最后一页
+            $target.append('<li><a herf="javascript:void(0)" data-page="last">' + options.last + '</a></li>');
             me.addClass();
         },
         // 获取中间页码起始值
@@ -98,36 +84,18 @@
                 end += me.size - me.active + 1;
             }
             // 起始页码不得小于等于1
-            if (start <= 1) {
-                start = 2;
+            if (start < 1) {
+                start = 1;
             }
             // 终止页码不得大于等于总页数
-            if (end >= options.total) {
-                end = options.total - 1;
+            if (end > options.total) {
+                end = options.total;
             }
             var size = {
                 start: start,
                 end: end
             }
             return size;
-        },
-        // 获取插入省略符位置
-        getEllipsis: function() {
-            var me = this,
-                $target = me.$target,
-                options = me.options,
-                page = [];
-            $target.find("li").each(function(i) {
-                var current = $(this).children("a[data-page]").data("page");
-                var next = $(this).next("li").children("a[data-page]").data("page");
-                // 如果页码不连续记录下位置
-                if (!isNaN(current) && !isNaN(next)) {
-                    if (current + 1 != next) {
-                        page.push(current);
-                    }
-                }
-            });
-            return page;
         },
         addClass: function() {
             var me = this,
@@ -138,10 +106,12 @@
             // 设置上一页样式
             if (me.active <= 1) {
                 $target.find('li>a[data-page="prev"]').addClass("disabled");
+                $target.find('li>a[data-page="first"]').addClass("disabled");
             }
             // 设置下一页样式
             if (me.active >= options.total) {
                 $target.find('li>a[data-page="next"]').addClass("disabled");
+                $target.find('li>a[data-page="last"]').addClass("disabled");
             }
         },
         // 页码点击事件
@@ -164,6 +134,10 @@
                     } else {
                         return;
                     }
+                } else if ($(this).data("page") === "first") {
+                    me.active = 1;
+                } else if ($(this).data("page") === "last") {
+                    me.active = me.options.total;
                 } else {
                     // 页码点击
                     if (!isNaN($(this).data("page"))) {
